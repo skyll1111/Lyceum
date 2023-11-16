3aa7d22225e45c90797c58a72546714614d92684
skyll1111 <98601854+skyll1111@users.noreply.github.com>
2023-11-16 17:17:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ class MyWindow(QMainWindow):
         scene.addItem(item)
 
     def getRandomColor(self):
-        return QColor(255, 255, 0)
+        return QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 
 if __name__ == '__main__':
